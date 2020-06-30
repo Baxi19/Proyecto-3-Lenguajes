928b0e7d0959c9ec461a92dbe2a4dcc03e41f0cd
Baxi19 <randald1991@gmail.com>
2020-06-30 14:46:19 -0600

    aulas y profesores estructurados

--- backend.pl
+++ backend.pl
@@ -2,70 +2,150 @@
 % Randald Villegas Brenes
 % 29/06/2020
 
+%profesores
+%findall(P->A->C->D->I, (profesor(P),apellidos(P,A), cedula(P,C),disponibilidad(P,D),imparte(P,I)),Res).
+profesor(alejandro).
+profesor(rocio).
+profesor(abel).
+profesor(diego).
+profesor(efren).
+profesor(lorena).
+profesor(jorge).
+profesor(marlen).
+profesor(leo).
+profesor(oscar).
+profesor(vera).
+profesor(rogelio).
 
-%horas de las lecciones
-leccion1("7:55","8:45").
-leccion2("8:50","9:40").
-leccion3("9:45","10:35").
-leccion4("10:40","11:30").
+apellidos(alejandro, "Alfaro Quesada").
+apellidos(rocio, "Quiros Oviedo").
+apellidos(abel, "Mendez Porras").
+apellidos(diego, "Rojas Vega").
+apellidos(efren, "Jimenez Delgado").
+apellidos(lorena, "Valerio Solis").
+apellidos(jorge, "Alfaro Velasco").
+apellidos(marlen, "Treviño Villalobos").
+apellidos(leo, "Viquez Acuña").
+apellidos(oscar, "Viquez Acuña").
+apellidos(vera, "Gamboa Guzman").
+apellidos(rogelio, "Gonzalez Quiros").
 
-leccion5("12:30","1:20").
-leccion6("1:25","2:15").
-leccion7("2:20","3:10").
-leccion8("3:15","4:05").
+cedula(alejandro, "1-846-952").
+cedula(rocio, "1-951-912").
+cedula(abel, "7-511-626").
+cedula(diego, "6-625-518").
+cedula(efren, "3-818-292").
+cedula(lorena, "6-514-651").
+cedula(jorge, "2-651-611").
+cedula(marlen, "6-515-321").
+cedula(leo, "3-216-566").
+cedula(oscar, "5-616-612").
+cedula(vera, "4-646-654").
+cedula(rogelio, "6-545-613").
+
+disponibilidad(alejandro, [lunes, martes]).
+disponibilidad(rocio,  [miercoles]).
+disponibilidad(abel,   [lunes, viernes]).
+disponibilidad(diego,  [jueves, viernes]).
+disponibilidad(efren,  [lunes, martes, miercoles, jueves]).
+disponibilidad(lorena, [lunes, martes, miercoles, jueves]).
+disponibilidad(jorge,  [lunes, martes, miercoles, jueves]).
+disponibilidad(marlen, [lunes, martes, miercoles, jueves]).
+disponibilidad(leo,    [lunes, martes, miercoles, jueves]).
+disponibilidad(oscar,  [lunes, martes, jueves, viernes]).
+disponibilidad(vera,   [lunes, martes, miercoles, jueves]).
+disponibilidad(rogelio,[lunes, martes, jueves, viernes]).
+
+imparte(alejandro, [requerimientos, diseno]).
+imparte(rocio,  [fundamentos]).
+imparte(abel,   [bases1, proyecto]).
+imparte(diego,  [fundamentos, proyecto]).
+imparte(efren,  [bases1, bases2, analisis]).
+imparte(lorena, [analisis, bases1, poo]).
+imparte(jorge,  [operativos, arquitectura]).
+imparte(marlen, [bases1, bases2, qa]).
+imparte(leo,    [bases2]).
+imparte(oscar,  [compiladores, lenguajes]).
+imparte(vera,   [introduccion, taller, computacion_sociedad]).
+imparte(rogelio,[ia, investigacion_operaciones, redes, electiva1, electiva2]).
+
+
+%aulas
+%findall(A->C->T, (aula(A),capacidad(A,C), tipo(A,T)),Res).
+aula(lab-01).
+aula(lab-02).
+aula(auc-01).
+aula(mvl-01).
+aula(e  -10).
+
+capacidad(lab-01,20).
+capacidad(lab-02,20).
+capacidad(auc-01,24).
+capacidad(mvl-01,20).
+capacidad(e-10,  24).
+
+tipo(lab-01,laboratorio).
+tipo(lab-02,laboratorio).
+tipo(auc-01,aula).
+tipo(mvl-01,laboratorio).
+tipo(e-10,  aula).
 
 
 %horarios semanales
-horario(lunes,"7:55","8:45").
-horario(lunes,"8:50","9:40").
-horario(lunes,"9:45","10:35").
-horario(lunes,"10:40","11:30").
-horario(lunes,"12:30","1:20").
-horario(lunes,"1:25","2:15").
-horario(lunes,"2:20","3:10").
-horario(lunes,"3:15","4:05").
+horario(l,"07:55","08:45").
+horario(l,"08:50","09:40").
+horario(l,"09:45","10:35").
+horario(l,"10:40","11:30").
+horario(l,"12:30","01:20").
+horario(l,"01:25","02:15").
+horario(l,"02:20","03:10").
+horario(l,"03:15","04:05").
 
-horario(martes,"7:55","8:45").
-horario(martes,"8:50","9:40").
-horario(martes,"9:45","10:35").
-horario(martes,"10:40","11:30").
-horario(martes,"12:30","1:20").
-horario(martes,"1:25","2:15").
-horario(martes,"2:20","3:10").
-horario(martes,"3:15","4:05").
+horario(k,"07:55","08:45").
+horario(k,"08:50","09:40").
+horario(k,"09:45","10:35").
+horario(k,"10:40","11:30").
+horario(k,"12:30","01:20").
+horario(k,"01:25","02:15").
+horario(k,"02:20","03:10").
+horario(k,"03:15","04:05").
+
+horario(m,"07:55","08:45").
+horario(m,"08:50","09:40").
+horario(m,"09:45","10:35").
+horario(m,"10:40","11:30").
+horario(m,"12:30","01:20").
+horario(m,"01:25","02:15").
+horario(m,"02:20","03:10").
+horario(m,"03:15","04:05").
+
+horario(j,"07:55","08:45").
+horario(j,"08:50","09:40").
+horario(j,"09:45","10:35").
+horario(j,"10:40","11:30").
+horario(j,"12:30","01:20").
+horario(j,"01:25","02:15").
+horario(j,"02:20","03:10").
+horario(j,"03:15","04:05").
+
+horario(v,"07:55","08:45").
+horario(v,"08:50","09:40").
+horario(v,"09:45","10:35").
+horario(v,"10:40","11:30").
+horario(v,"12:30","01:20").
+horario(v,"01:25","02:15").
+horario(v,"02:20","03:10").
+horario(v,"03:15","04:05").
 
 
-horario(miercoles,"7:55","8:45").
-horario(miercoles,"8:50","9:40").
-horario(miercoles,"9:45","10:35").
-horario(miercoles,"10:40","11:30").
-horario(miercoles,"12:30","1:20").
-horario(miercoles,"1:25","2:15").
-horario(miercoles,"2:20","3:10").
-horario(miercoles,"3:15","4:05").
-
-horario(jueves,"7:55","8:45").
-horario(jueves,"8:50","9:40").
-horario(jueves,"9:45","10:35").
-horario(jueves,"10:40","11:30").
-horario(jueves,"12:30","1:20").
-horario(jueves,"1:25","2:15").
-horario(jueves,"2:20","3:10").
-horario(jueves,"3:15","4:05").
-
-horario(viernes,"7:55","8:45").
-horario(viernes,"8:50","9:40").
-horario(viernes,"9:45","10:35").
-horario(viernes,"10:40","11:30").
-horario(viernes,"12:30","1:20").
-horario(viernes,"1:25","2:15").
-horario(viernes,"2:20","3:10").
-horario(viernes,"3:15","4:05").
+%buscarHorario(X, 01:25","02:15").
+%buscarHorario(lunes, X, Y).
 
 
-% profesor(nombre, cédula, [disponibilidad de horario], [cursos que
-% imparte])
-profesor("Alejandro Alfaro Quesada", 209870845, [], ["Analisis Y Diseño De Algoritmos"]).
+buscarHorario(Dia, Inicio, Fin) :-
+    horario(Dia, Inicio, Fin).
+
+
 
 
 
@@ -81,3 +161,5 @@ curso("CA3125","Analisis Y Diseño De Algoritmos",3.5, 3).
 
 
 curso(prolog,horario(lunes,9,12),profesor(eduardo,morales),lugar(ccc,8301)).
+
+
