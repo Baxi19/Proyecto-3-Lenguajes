c52076162f824e674704c1cbdd9fd20b87ac4ed5
Baxi19 <randald1991@gmail.com>
2020-07-02 18:03:29 -0600

    Clases casi listas

---
+++ Proyecto-4-Lenguajes/src/proyecto/pkg4/lenguajes/Proyecto4Lenguajes.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg4.lenguajes;
+
+import org.jpl7.Query;
+
+
+/**
+ *
+ * @author Baxi
+ */
+public class Proyecto4Lenguajes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String t1 = "consult('backend.pl').";
+        Query q1 = new Query(t1);
+    }
+    
+}
